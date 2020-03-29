1f223914e9f80a32ba99737c629cd4dbdb02b1d4
David Montes <david.montes@sjsu.edu>
2020-03-28 23:44:54 -0700

    fixed map and apikey problem

--- app/src/main/java/com/example/dronetracker2/ui/map/MapFragment.java
+++ app/src/main/java/com/example/dronetracker2/ui/map/MapFragment.java
@@ -4,29 +4,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.dronetracker2.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
-
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     private MapViewModel mapViewModel;
+    private View myview;
+    private MapView mapView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        mapViewModel =
-                ViewModelProviders.of(this).get(MapViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_map, container, false);
-//        final TextView textView = root.findViewById(R.id.text_notifications);
-//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-        return root;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        myview = inflater.inflate(R.layout.fragment_map, container, false);
+        return myview;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = myview.findViewById(R.id.map);
+
+        if (mapView != null)
+        {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng marker = new LatLng(37.3354550, -121.8850220);
+
+        googleMap.addMarker(new MarkerOptions().position(marker).title("Marker in San Jose"));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
     }
 }
