cc06829c359b1a966ca2020c2b2c836e013b9b02
David Montes <david.montes@sjsu.edu>
2020-03-28 21:20:40 -0700

    init2

---
+++ app/src/main/java/com/example/dronetracker2/ui/details/DetailsFragment.java
@@ -0,0 +1,35 @@
+package com.example.dronetracker2.ui.details;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.dronetracker2.R;
+
+public class DetailsFragment extends Fragment {
+
+    private DetailsViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DetailsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_details, container, false);
+        //final TextView textView = root.findViewById(R.id.text_dashboard);
+        //dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //    @Override
+        //    public void onChanged(@Nullable String s) {
+        //        textView.setText(s);
+        //    }
+        //});
+        return root;
+    }
+}
